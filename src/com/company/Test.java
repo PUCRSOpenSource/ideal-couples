a274b715fc2db54a364813190bed3d6097a3f1f5
Matthias Nunes <matthiasnunes@gmail.com>
2018-05-01 19:30:25 -0300

    Adds homem não tão gostoso
    Co-authored-by: Marina Barros <marina.barros@acad.pucrs.br>
    Co-authored-by: Giovanni Cuper <giovanni_cuper@hotmail.com>

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -6,13 +6,22 @@ public class Test {
     public static void main(String[] args) {
         ArrayList<Integer> preferenciasHomem = new ArrayList<>();
         preferenciasHomem.add(1);
+        Agente homemGostoso = new Agente(1, preferenciasHomem, Genero.HOMEM);
+
+        ArrayList<Integer> preferenciasHomem2 = new ArrayList<>();
+        preferenciasHomem.add(1);
+        Agente homemNaoTaoGostoso = new Agente(2, preferenciasHomem, Genero.HOMEM);
+
         ArrayList<Integer> preferenciasMulher = new ArrayList<>();
         preferenciasMulher.add(1);
-        Agente homem = new Agente(1, preferenciasHomem, Genero.HOMEM);
+        preferenciasMulher.add(2);
         Agente mulher = new Agente(1, preferenciasMulher, Genero.MULHER);
+
         ArrayList<Agente> agentes = new ArrayList<>();
-        agentes.add(homem);
+        agentes.add(homemGostoso);
+        agentes.add(homemNaoTaoGostoso);
         agentes.add(mulher);
+
         Ambiente amb = new Ambiente(10, agentes, 1);
         amb.activateAgentes();
     }
